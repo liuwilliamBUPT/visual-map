44483d8e242613576a0192970c20bfead641ff37
William <liuwilliam.zh@gmail.com>
2023-05-04 01:23:38 +0800

    init commit

---
+++ .history/src/components/HelloWorld_20230503191446.vue
@@ -0,0 +1,102 @@
+<script setup lang="ts">
+import CN from '../assets/100000_full.json'
+
+import * as echarts from 'echarts/core';
+import { GridComponent, GridComponentOption } from 'echarts/components';
+import { BarChart, BarSeriesOption } from 'echarts/charts';
+import { UniversalTransition } from 'echarts/features';
+import { CanvasRenderer } from 'echarts/renderers';
+import { onMounted } from 'vue';
+
+echarts.use([GridComponent, BarChart, CanvasRenderer, UniversalTransition]);
+
+type EChartsOption = echarts.ComposeOption<
+  GridComponentOption | BarSeriesOption
+>;
+
+onMounted(() => {
+  let chartDom = document.getElementById('main')!;
+  let myChart = echarts.init(chartDom);
+  let option: EChartsOption;
+
+  myChart.showLoading();
+
+  myChart.hideLoading()
+  echarts.registerMap('CN', CN!, {
+
+  })
+
+  let data = [
+    { name: '北京市', value: 4822023 }
+  ];
+
+  const mapOption = {
+    visualMap: {
+        left: 'right',
+        min: 500000,
+        max: 38000000,
+        inRange: {
+          // prettier-ignore
+          color: ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026']
+        },
+        text: ['High', 'Low'],
+        calculable: true
+      },
+      series: [
+        {
+          id: 'population',
+          type: 'map',
+          roam: true,
+          map: 'USA',
+          animationDurationUpdate: 1000,
+          universalTransition: true,
+          data: data
+        }
+      ]
+  }
+
+  const barOption = {
+      xAxis: {
+        type: 'value'
+      },
+      yAxis: {
+        type: 'category',
+        axisLabel: {
+          rotate: 30
+        },
+        data: data.map(function (item) {
+          return item.name;
+        })
+      },
+      animationDurationUpdate: 1000,
+      series: {
+        type: 'bar',
+        id: 'population',
+        data: data.map(function (item) {
+          return item.value;
+        }),
+        universalTransition: true
+      }
+    };
+
+  let currentOption = mapOption;
+  myChart.setOption(mapOption);
+
+  setInterval(function () {
+    currentOption = currentOption === mapOption ? barOption : mapOption;
+    myChart.setOption(currentOption, true);
+  }, 2000);
+
+  option! && myChart.setOption(option);
+}),
+</script>
+
+<template>
+  <div id="main"></div>
+</template>
+
+<style scoped>
+#main {
+  height: 500px;
+}
+</style>
---
+++ .history/src/components/HelloWorld_20230504011945.vue
@@ -0,0 +1,125 @@
+<script setup lang="ts">
+import CN from "../assets/100000_full.json";
+import { onMounted } from "vue";
+import * as echarts from "echarts/core";
+import {
+  TitleComponent,
+  TitleComponentOption,
+  ToolboxComponent,
+  ToolboxComponentOption,
+  TooltipComponent,
+  TooltipComponentOption,
+  VisualMapComponent,
+  VisualMapComponentOption,
+  GeoComponent,
+  GeoComponentOption,
+} from "echarts/components";
+import { MapChart, MapSeriesOption } from "echarts/charts";
+import { CanvasRenderer } from "echarts/renderers";
+
+echarts.use([
+  TitleComponent,
+  ToolboxComponent,
+  TooltipComponent,
+  VisualMapComponent,
+  GeoComponent,
+  MapChart,
+  CanvasRenderer,
+]);
+
+type EChartsOption = echarts.ComposeOption<
+  | TitleComponentOption
+  | ToolboxComponentOption
+  | TooltipComponentOption
+  | VisualMapComponentOption
+  | GeoComponentOption
+  | MapSeriesOption
+>;
+
+onMounted(() => {
+  let chartDom = document.getElementById("main")!;
+  let myChart = echarts.init(chartDom);
+  let option: EChartsOption;
+
+  myChart.showLoading();
+
+  myChart.hideLoading();
+  echarts.registerMap("CN", CN as any, {});
+
+  option = {
+    title: {
+      text: "USA Population Estimates (2012)",
+      subtext: "Data from www.census.gov",
+      sublink: "http://www.census.gov/popest/data/datasets.html",
+      left: "right",
+    },
+    tooltip: {
+      trigger: "item",
+      showDelay: 0,
+      transitionDuration: 0.2,
+    },
+    visualMap: {
+      left: "right",
+      min: 500000,
+      max: 38000000,
+      inRange: {
+        color: [
+          "#313695",
+          "#4575b4",
+          "#74add1",
+          "#abd9e9",
+          "#e0f3f8",
+          "#ffffbf",
+          "#fee090",
+          "#fdae61",
+          "#f46d43",
+          "#d73027",
+          "#a50026",
+        ],
+      },
+      text: ["High", "Low"],
+      calculable: true,
+    },
+    toolbox: {
+      show: true,
+      //orient: 'vertical',
+      left: "left",
+      top: "top",
+      feature: {
+        dataView: { readOnly: false },
+        restore: {},
+        saveAsImage: {},
+      },
+    },
+    series: [
+      {
+        name: "USA PopEstimates",
+        type: "map",
+        roam: true,
+        map: "CN",
+        emphasis: {
+          label: {
+            show: true,
+          },
+        },
+        data: [{ name: "北京市", value: 4822023 }],
+      },
+    ],
+  };
+
+  myChart.setOption(option);
+
+  option && myChart.setOption(option);
+});
+</script>
+
+<template>
+  <div id="main"></div>
+</template>
+
+<style scoped>
+#main {
+  height: 1000px;
+  width: 1000px;
+}
+</style>
